d326696c2eb2d95ce1b5ba44170197859018f95a
alexis8buban@gmail.com <alexis8buban@gmail.com>
2025-08-13 10:35:41 +0800

    Added collection for entity

---
+++ app/src/main/java/com/jfinex/admin/data/local/collection/Collection.kt
@@ -0,0 +1,12 @@
+package com.jfinex.admin.data.local.collection
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "collections")
+data class Collection(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0,
+    val name: String,
+    val categories: List<String>?
+)

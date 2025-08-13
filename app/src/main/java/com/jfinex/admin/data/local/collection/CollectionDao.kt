8e860c0dcab35ce85da17c07d201db8e357ffce7
alexis8buban@gmail.com <alexis8buban@gmail.com>
2025-08-13 10:36:10 +0800

    Added collection dao

---
+++ app/src/main/java/com/jfinex/admin/data/local/collection/CollectionDao.kt
@@ -0,0 +1,21 @@
+package com.jfinex.admin.data.local.collection
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface CollectionDao {
+
+    @Insert
+    suspend fun addCollection(collection: Collection)
+
+    @Delete
+    suspend fun deleteCollection(collection: Collection)
+
+    @Query("SELECT * FROM collections")
+    fun getAllCollections(): Flow<List<Collection>>
+
+}

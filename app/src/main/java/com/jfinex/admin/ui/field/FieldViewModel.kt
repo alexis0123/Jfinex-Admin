5d0c74f82fa59422e35e264d0ed1b611f068bf8f
alexis8buban@gmail.com <alexis8buban@gmail.com>
2025-08-19 12:00:44 +0800

    removed hardcoded data

--- app/src/main/java/com/jfinex/admin/ui/field/FieldViewModel.kt
+++ app/src/main/java/com/jfinex/admin/ui/field/FieldViewModel.kt
@@ -3,29 +3,12 @@ package com.jfinex.admin.ui.field
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
 
 @HiltViewModel
 class FieldViewModel @Inject constructor(private val repo: FieldsRepository): ViewModel() {
-    private val _fields = MutableStateFlow<List<Field>>(
-        listOf(
-            Field(name = "Membership"),
-            Field(
-                name = "T-shirt",
-                category = listOf("Small", "Medium", "Large")
-            ),
-            Field(
-                name = "T-shirt",
-                category = listOf("S", "M", "L")
-            ),
-            Field(
-                name = "T-shirt",
-                category = listOf("Small", "Medium", "Large")
-            )
-        )
-    )
+    private val _fields = MutableStateFlow<List<Field>>(emptyList())
     val fields = _fields.asStateFlow()
 
     fun addField(field: Field) {

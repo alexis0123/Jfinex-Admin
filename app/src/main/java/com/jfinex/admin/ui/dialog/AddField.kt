b367e5682e182fa4003de8dcbc06f3eb7e7a24e0
alexis8buban@gmail.com <alexis8buban@gmail.com>
2025-08-19 12:01:46 +0800

    removed unnecessary imports and added new one

--- app/src/main/java/com/jfinex/admin/ui/dialog/AddField.kt
+++ app/src/main/java/com/jfinex/admin/ui/dialog/AddField.kt
@@ -1,7 +1,6 @@
 package com.jfinex.admin.ui.dialog
 
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box

47364627e4a2740bf51d1d611e59459793548f8c
alexis8buban@gmail.com <alexis8buban@gmail.com>
2025-08-19 10:47:01 +0800

    Refactored card

--- app/src/main/java/com/jfinex/admin/ui/dialog/ShowFile.kt
+++ app/src/main/java/com/jfinex/admin/ui/dialog/ShowFile.kt
@@ -32,6 +32,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
+import com.jfinex.admin.ui.dialog.components.StyledCard
 
 @Composable
 fun ShowFile(
@@ -42,98 +43,77 @@ fun ShowFile(
     val rows by viewModel.validRows.collectAsState()
 
     Dialog(onDismissRequest = {}) {
-        Surface(
-            modifier = Modifier
-                .width(400.dp)
-                .height(500.dp),
-            color = MaterialTheme.colorScheme.background,
-            shape = RoundedCornerShape(15.dp),
-            border = BorderStroke(1.dp, Color.Black),
-            shadowElevation = 2.dp
-        ) {
-            Column(
+
+        StyledCard {
+            Box(
                 modifier = Modifier
-                    .fillMaxSize()
-                    .padding(bottom = 10.dp),
-                verticalArrangement = Arrangement.SpaceBetween,
-                horizontalAlignment = Alignment.CenterHorizontally
+                    .height(360.dp)
+                    .width(285.dp)
+                    .border(1.dp, Color.Black, RoundedCornerShape(10.dp))
+                    .padding(10.dp)
             ) {
-                Box(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(50.dp)
-                        .background(MaterialTheme.colorScheme.secondary)
-                ) {}
-                Box(
-                    modifier = Modifier
-                        .height(360.dp)
-                        .width(285.dp)
-                        .border(1.dp, Color.Black, RoundedCornerShape(10.dp))
+                Column(
+                    Modifier
+                        .fillMaxSize()
                         .padding(10.dp)
-                ){
-                    Column(
-                        Modifier
-                            .fillMaxSize()
-                            .padding(10.dp)
+                ) {
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
                     ) {
-                        Row(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                        ) {
-                            Text(
-                                text = "Number of Rows: ${rows.size}",
-                                fontWeight = FontWeight.Bold
-                            )
-                        }
-                        Row(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                        ) {
-                            Text(
-                                text = "Block",
-                                modifier = Modifier.weight(0.3f),
-                                fontWeight = FontWeight.Bold
-                            )
-                            Text(
-                                text = "Name",
-                                modifier = Modifier.weight(1f),
-                                fontWeight = FontWeight.Bold
-                            )
-                        }
-                        HorizontalDivider(color = Color.Black)
-                        LazyColumn {
-                            items(rows) { row ->
-                                Row(
-                                    Modifier
-                                        .fillMaxWidth()
-                                        .padding(vertical = 5.dp)
-                                ) {
-                                    row.forEachIndexed { index, item ->
-                                        val weight = if (index == 0) 0.3f else 1f
-                                        Text(
-                                            text = item,
-                                            modifier = Modifier.weight(weight),
-                                            maxLines = 1,
-                                            overflow = TextOverflow.Ellipsis
-                                        )
-                                    }
+                        Text(
+                            text = "Number of Rows: ${rows.size}",
+                            fontWeight = FontWeight.Bold
+                        )
+                    }
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                    ) {
+                        Text(
+                            text = "Block",
+                            modifier = Modifier.weight(0.3f),
+                            fontWeight = FontWeight.Bold
+                        )
+                        Text(
+                            text = "Name",
+                            modifier = Modifier.weight(1f),
+                            fontWeight = FontWeight.Bold
+                        )
+                    }
+                    HorizontalDivider(color = Color.Black)
+                    LazyColumn {
+                        items(rows) { row ->
+                            Row(
+                                Modifier
+                                    .fillMaxWidth()
+                                    .padding(vertical = 5.dp)
+                            ) {
+                                row.forEachIndexed { index, item ->
+                                    val weight = if (index == 0) 0.3f else 1f
+                                    Text(
+                                        text = item,
+                                        modifier = Modifier.weight(weight),
+                                        maxLines = 1,
+                                        overflow = TextOverflow.Ellipsis
+                                    )
                                 }
                             }
                         }
                     }
                 }
-                OutlinedButton(
-                    onClick = onDismiss,
-                    shape = RoundedCornerShape(10.dp),
-                    modifier = Modifier
-                        .width(285.dp)
-                        .height(50.dp)
-                ) {
-                    Text(
-                        text = "Ok",
-                        color = Color.Black
-                    )
-                }
+            }
+            OutlinedButton(
+                onClick = onDismiss,
+                shape = RoundedCornerShape(10.dp),
+                modifier = Modifier
+                    .width(285.dp)
+                    .height(50.dp)
+            ) {
+                Text(
+                    text = "Ok",
+                    color = Color.Black
+                )
             }
         }
     }

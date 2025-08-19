b3fc68bd06f926010d7d6272c7e041ac41efbb61
alexis8buban@gmail.com <alexis8buban@gmail.com>
2025-08-19 11:24:12 +0800

    Added add category textfield with buttons for later

--- app/src/main/java/com/jfinex/admin/ui/dialog/AddField.kt
+++ app/src/main/java/com/jfinex/admin/ui/dialog/AddField.kt
@@ -10,20 +10,27 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -36,10 +43,12 @@ fun AddFieldDialog(
     viewModel: FieldViewModel = hiltViewModel()
 ) {
     var fieldNameText by remember { mutableStateOf("") }
+    var categoryText by remember { mutableStateOf("") }
 
     Dialog(onDismissRequest = {}) {
         StyledCard {
             Column(
+                verticalArrangement = Arrangement.spacedBy(10.dp),
                 modifier = Modifier
                     .height(360.dp)
                     .width(285.dp)
@@ -51,8 +60,66 @@ fun AddFieldDialog(
                     onValueChange = { fieldNameText = it },
                     singleLine = true,
                     shape = RoundedCornerShape(10.dp),
-                    label = { Text("Field Name") }
+                    label = { Text("Field Name") },
+                    keyboardOptions = KeyboardOptions(
+                        capitalization = KeyboardCapitalization.Words
+                    ),
+                    colors = TextFieldDefaults.colors(
+                        focusedContainerColor = Color.Transparent,
+                        unfocusedContainerColor = Color.Transparent,
+                        disabledContainerColor = Color.Transparent,
+                        focusedIndicatorColor = MaterialTheme.colorScheme.secondary,
+                        unfocusedIndicatorColor = Color.DarkGray,
+                        focusedLabelColor = MaterialTheme.colorScheme.secondary,
+                        unfocusedLabelColor = Color.DarkGray,
+                        cursorColor = MaterialTheme.colorScheme.secondary
+                    )
                 )
+
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+
+                    OutlinedTextField(
+                        value = categoryText,
+                        onValueChange = { categoryText = it },
+                        singleLine = true,
+                        shape = RoundedCornerShape(10.dp),
+                        label = { Text("Category Name") },
+                        keyboardOptions = KeyboardOptions(
+                            capitalization = KeyboardCapitalization.Words
+                        ),
+                        colors = TextFieldDefaults.colors(
+                            focusedContainerColor = Color.Transparent,
+                            unfocusedContainerColor = Color.Transparent,
+                            disabledContainerColor = Color.Transparent,
+                            focusedIndicatorColor = MaterialTheme.colorScheme.secondary,
+                            unfocusedIndicatorColor = Color.DarkGray,
+                            focusedLabelColor = MaterialTheme.colorScheme.secondary,
+                            unfocusedLabelColor = Color.DarkGray,
+                            cursorColor = MaterialTheme.colorScheme.secondary
+                        ),
+                        modifier = Modifier.width(170.dp)
+                    )
+
+                    Button(
+                        onClick = {},
+                        shape = RoundedCornerShape(10.dp),
+                        border = BorderStroke(1.dp, Color.Black),
+                        colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent),
+                        modifier = Modifier
+                            .width(85.dp)
+                            .height(55.dp)
+                            .offset(y = 4.dp)
+                    ) {
+                        Text(
+                            text = "Add",
+                            color = Color.Black
+                        )
+                    }
+                }
             }
 
             Row(
@@ -68,7 +135,7 @@ fun AddFieldDialog(
                         .height(50.dp)
                 ) {
                     Text(
-                        text = "Add",
+                        text = "Save Field",
                         color = Color.Black
                     )
                 }

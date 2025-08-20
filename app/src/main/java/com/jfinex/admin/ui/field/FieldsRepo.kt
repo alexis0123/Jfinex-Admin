1ccef291367a545d94150780bd3775c86c094ecd
alexis8buban@gmail.com <alexis8buban@gmail.com>
2025-08-20 19:55:57 +0800

    Added serializable annotation for use later

--- app/src/main/java/com/jfinex/admin/ui/field/FieldsRepo.kt
+++ app/src/main/java/com/jfinex/admin/ui/field/FieldsRepo.kt
@@ -1,7 +1,9 @@
 package com.jfinex.admin.ui.field
 
+import kotlinx.serialization.Serializable
 import javax.inject.Inject
 
+@Serializable
 data class Field(
     val name: String,
     val category: List<String> = emptyList()

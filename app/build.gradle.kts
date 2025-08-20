44f64d20f71fa679ef735d86a05da3d4b9a16789
alexis8buban@gmail.com <alexis8buban@gmail.com>
2025-08-20 19:54:54 +0800

    Added dependancies for json export config later

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.ksp)
     alias(libs.plugins.hilt)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
@@ -67,4 +68,5 @@ dependencies {
     implementation(libs.opencsv)
     implementation(libs.compose.foundation.pager)
     implementation(libs.androidx.material.icons.extended)
+    implementation(libs.kotlinx.serialization.json)
 }

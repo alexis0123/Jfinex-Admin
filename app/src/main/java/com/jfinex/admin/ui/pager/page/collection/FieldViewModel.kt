3443905cfcf159f59d22ac7613451ae001607296
alexis8buban@gmail.com <alexis8buban@gmail.com>
2025-08-23 09:47:04 +0800

    added viewmodel for field

---
+++ app/src/main/java/com/jfinex/admin/ui/pager/page/collection/FieldViewModel.kt
@@ -0,0 +1,22 @@
+package com.jfinex.admin.ui.pager.page.collection
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.jfinex.admin.data.local.fields.Field
+import com.jfinex.admin.data.local.fields.FieldRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class FieldViewModel @Inject constructor(
+    private val repo: FieldRepository): ViewModel() {
+
+    val fields: StateFlow<List<Field>> = repo.getAll()
+        .stateIn(viewModelScope, SharingStarted.Lazily, emptyList())
+
+    }

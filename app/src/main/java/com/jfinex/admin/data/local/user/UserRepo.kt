bac6b823d9c74f8e73fa204ce3337c67f4acec51
alexis8buban@gmail.com <alexis8buban@gmail.com>
2025-08-25 18:35:53 +0800

    added set user

--- app/src/main/java/com/jfinex/admin/data/local/user/UserRepo.kt
+++ app/src/main/java/com/jfinex/admin/data/local/user/UserRepo.kt
@@ -6,4 +6,5 @@ import javax.inject.Inject
 class UserRepo @Inject constructor(private val dao: UserDao) {
     fun getUser(): Flow<User?> = dao.getUser()
 
+    suspend fun setUser(user: User) = dao.addUser(user)
 }

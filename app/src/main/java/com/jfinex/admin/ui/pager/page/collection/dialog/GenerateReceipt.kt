594bb523a5b858e5879979f344e978bb6f07e3dd
alexis8buban@gmail.com <alexis8buban@gmail.com>
2025-08-29 11:46:22 +0800

    refactored and used latest viewModel instead

--- app/src/main/java/com/jfinex/admin/ui/pager/page/collection/dialog/GenerateReceipt.kt
+++ app/src/main/java/com/jfinex/admin/ui/pager/page/collection/dialog/GenerateReceipt.kt
@@ -10,14 +10,23 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Dangerous
+import androidx.compose.material.icons.filled.ErrorOutline
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -26,19 +35,20 @@ import com.jfinex.admin.data.local.components.formattedDate
 import com.jfinex.admin.data.local.features.collection.CollectionViewModel
 import com.jfinex.admin.data.local.features.students.Student
 import com.jfinex.admin.ui.dialog.components.StyledCard
-import java.time.LocalDate
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 
 @Composable
 fun GenerateReceipt(
     onDismiss: () -> Unit,
-    officerName: String,
-    student: Student,
-    fields: Map<String, String>
+    viewModel: ReceiptGeneratorViewModel = hiltViewModel()
 ) {
-    val date = dateToday()
+
+    val newCollections by viewModel.newCollections.collectAsState()
+    val existing by viewModel.alreadyExists.collectAsState()
 
     Dialog(onDismissRequest = onDismiss) {
-        StyledCard(title = "Receipts") {
+        StyledCard(title = "Receipts Generated Successfully") {
             Column(
                 verticalArrangement = Arrangement.SpaceBetween,
                 horizontalAlignment = Alignment.CenterHorizontally,
@@ -48,11 +58,7 @@ fun GenerateReceipt(
                     verticalArrangement = Arrangement.spacedBy(15.dp),
                     modifier = Modifier.fillMaxSize()
                 ) {
-                    items(fields.entries.toList()) { field ->
-                        val item = field.key
-                        val category = field.value
-                        val receiptNumber = student.receiptNumber[item]!!
-
+                    items(newCollections) { collection ->
                         Box(
                             modifier = Modifier
                                 .fillMaxWidth()
@@ -65,17 +71,77 @@ fun GenerateReceipt(
                                     .fillMaxSize()
                                     .padding(10.dp)
                             ) {
-                                Text(student.block)
                                 Row(
                                     modifier = Modifier.fillMaxWidth(),
                                     horizontalArrangement = Arrangement.SpaceBetween
                                 ) {
-                                    Text("$receiptNumber")
-                                    Text(formattedDate(date))
+                                    Text(collection.block)
+                                    Text(
+                                        text = "Receipt",
+                                        fontWeight = FontWeight.Bold
+                                    )
+                                    Icon(
+                                        imageVector = Icons.Default.Check,
+                                        contentDescription = "Check",
+                                        tint = Color.Gray,
+                                        modifier = Modifier
+                                            .size(20.dp)
+                                    )
                                 }
-                                Text("   ${student.name}")
+                                Row(
+                                    modifier = Modifier.fillMaxWidth(),
+                                    horizontalArrangement = Arrangement.SpaceBetween
+                                ) {
+                                    Text("${collection.receiptNumber}")
+                                    Text(formattedDate(collection.date))
+                                }
+                                Text("   ${collection.name}")
                                 Text(
-                                    text = "   $item${if (category != "Paid" && category.isNotBlank()) " ($category)" else ""}"
+                                    text = "   ${collection.item}${
+                                        if (collection.category != "Paid" && collection.category!!
+                                                .isNotBlank()) " (${collection.category})" else ""}"
+                                )
+                            }
+                        }
+                    }
+                    items(existing) { collection ->
+                        Box(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(100.dp)
+                                .background(
+                                    MaterialTheme.colorScheme.primary.copy(alpha = 0.1f),
+                                    RoundedCornerShape(10.dp)
+                                )
+                                .border(1.dp, Color.Black, RoundedCornerShape(10.dp))
+                        ) {
+                            Column(
+                                modifier = Modifier
+                                    .fillMaxSize()
+                                    .padding(10.dp)
+                            ) {
+                                Row(
+                                    modifier = Modifier.fillMaxWidth(),
+                                    horizontalArrangement = Arrangement.SpaceBetween
+                                ) {
+                                    Text(collection.block)
+                                    Text(
+                                        text = "Receipt Already Exist",
+                                        fontWeight = FontWeight.Bold
+                                    )
+                                    Icon(
+                                        imageVector = Icons.Default.ErrorOutline,
+                                        contentDescription = "Error",
+                                        tint = Color.Black,
+                                        modifier = Modifier
+                                            .size(25.dp)
+                                    )
+                                }
+                                Text("   ${collection.name}")
+                                Text(
+                                    text = "   ${collection.item}${
+                                        if (collection.category != "Paid" && collection.category!!
+                                                .isNotBlank()) " (${collection.category})" else ""}"
                                 )
                             }
                         }

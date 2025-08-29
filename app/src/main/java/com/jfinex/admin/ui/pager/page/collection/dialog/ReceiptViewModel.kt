edfba04784c72a6fadb93bcf870304d0b1acd9ea
alexis8buban@gmail.com <alexis8buban@gmail.com>
2025-08-29 11:37:16 +0800

    Added getreceipt by name, block and item

--- app/src/main/java/com/jfinex/admin/ui/pager/page/collection/dialog/ReceiptViewModel.kt
+++ app/src/main/java/com/jfinex/admin/ui/pager/page/collection/dialog/ReceiptViewModel.kt
@@ -5,6 +5,8 @@ import androidx.lifecycle.viewModelScope
 import com.jfinex.admin.data.local.features.receipt.Receipt
 import com.jfinex.admin.data.local.features.receipt.ReceiptRepo
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import java.time.LocalDate
 import javax.inject.Inject
@@ -13,6 +15,9 @@ import javax.inject.Inject
 class ReceiptViewModel @Inject constructor(
     private val repo: ReceiptRepo
 ): ViewModel() {
+
+    private var _receipt = MutableStateFlow<Receipt?>(null)
+    val receipt: StateFlow<Receipt?> = _receipt
 
     fun generateReceipt(
         date: LocalDate,
@@ -36,8 +41,30 @@ class ReceiptViewModel @Inject constructor(
         }
     }
 
-    suspend fun removeReceipt(receipt: Receipt) {
-        repo.removeReceipt(receipt = receipt)
+    fun removeReceipt(receipt: Receipt) {
+        viewModelScope.launch {
+            repo.removeReceipt(receipt = receipt)
+        }
     }
+
+    fun getReceipt(name: String, block: String, item: String) {
+        viewModelScope.launch {
+            _receipt.value = repo.getReceipt(name = name, block = block, item = item)
+        }
+    }
+
+    fun clear() {
+        _receipt.value = null
+    }
+
+    fun removeByNameBlockItem(name: String, block: String, item: String) {
+        viewModelScope.launch {
+            val receipt = repo.getReceipt(name, block, item)
+            if (receipt != null) {
+                repo.removeReceipt(receipt)
+            }
+        }
+    }
+
 
 }

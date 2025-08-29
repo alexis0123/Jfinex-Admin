ef9c63a8c107f6d42d419bebb165d39a9f845dfb
alexis8buban@gmail.com <alexis8buban@gmail.com>
2025-08-29 11:45:13 +0800

    added generate receipt to process both collection and receipt.. big help for generateReceipt dialog later

---
+++ app/src/main/java/com/jfinex/admin/ui/pager/page/collection/dialog/ReceiptGeneratorViewModel.kt
@@ -0,0 +1,85 @@
+package com.jfinex.admin.ui.pager.page.collection.dialog
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.jfinex.admin.data.local.components.dateToday
+import com.jfinex.admin.data.local.features.collection.Collection
+import com.jfinex.admin.data.local.features.collection.CollectionRepo
+import com.jfinex.admin.data.local.features.collection.CollectionViewModel
+import com.jfinex.admin.data.local.features.receipt.Receipt
+import com.jfinex.admin.data.local.features.receipt.ReceiptRepo
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import java.time.LocalDate
+import javax.inject.Inject
+
+@HiltViewModel
+class ReceiptGeneratorViewModel @Inject constructor(
+    private val receiptRepo: ReceiptRepo,
+    private val collectionRepo: CollectionRepo
+): ViewModel() {
+    private val _newCollections = MutableStateFlow<List<Collection>>(emptyList())
+    val newCollections: StateFlow<List<Collection>> = _newCollections
+
+    private val _alreadyExists = MutableStateFlow<List<Collection>>(emptyList())
+    val alreadyExists: StateFlow<List<Collection>> = _alreadyExists
+
+    fun generateReceipt(
+        name: String,
+        block: String,
+        officerName: String,
+        item: String,
+        category: String,
+        receiptNumber: Int
+    ) {
+        viewModelScope.launch {
+            val existing = receiptRepo.getReceipt(
+                name = name,
+                block = block,
+                item = item
+            )
+            val collection = Collection(
+                type = "Receipt",
+                date = dateToday(),
+                name = name,
+                block = block,
+                officerName = officerName,
+                item = item,
+                category = category,
+                receiptNumber = receiptNumber
+            )
+            if (existing == null) {
+                receiptRepo.addReceipt(
+                    date = dateToday(),
+                    name = name,
+                    block = block,
+                    officerName = officerName,
+                    item = item,
+                    category = category,
+                    receiptNumber = receiptNumber
+                )
+                collectionRepo.addCollection(
+                    type = "Receipt",
+                    date = dateToday(),
+                    name = name,
+                    block = block,
+                    officerName = officerName,
+                    item = item,
+                    category = category,
+                    receiptNumber = receiptNumber
+                )
+                _newCollections.value = _newCollections.value + collection
+            } else {
+                _alreadyExists.value = _alreadyExists.value + collection
+            }
+        }
+    }
+
+    fun clear() {
+        _newCollections.value = emptyList()
+        _alreadyExists.value = emptyList()
+    }
+
+}
